967f4a1450315d957388ab34f7add71375cdc0f1
Enchan <enchant0130@gmail.com>
2021-01-18 21:02:40 +0900

    [Add] Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#
+# Tweepyを使ってみる
+#
+import tweepy
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Ctrl+C")
+        exit(0)
