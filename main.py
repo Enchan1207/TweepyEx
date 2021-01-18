af271a80342d2cb8de37cd78956c4f9bf0fd9e09
Enchan <enchant0130@gmail.com>
2021-01-18 21:15:50 +0900

    [Update] API Prepare

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 #
 # Tweepyを使ってみる
 #
+import dotenv, os, json
 import tweepy
 
 def main():
+    # load environmental variables from .env
+    dotenv.load_dotenv()
 
-    pass
+    # prepare
+    consumer_key = os.getenv("CLIENT_ID")
+    consumer_secret = os.getenv("CLIENT_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
+    # call API Endpoints
+    api = tweepy.API(auth)
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
+  
 if __name__ == "__main__":
     try:
         main()
