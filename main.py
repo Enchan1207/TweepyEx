96a0f9d94ebdb0cd1745c2b2d3742c6a2e666814
Enchan <enchant0130@gmail.com>
2021-01-18 22:46:04 +0900

    [Update] dump tweet object

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ def main():
     auth.set_access_token(access_token, access_token_secret)
 
     # call API Endpoints
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     public_tweets = api.home_timeline()
     for tweet in public_tweets:
         print(tweet.text)
+
+    with open("logs/tweetObject.json", "w") as f:
+        f.write(json.dumps(public_tweets[0]._json,  ensure_ascii=False, indent=4))
   
 if __name__ == "__main__":
     try:
